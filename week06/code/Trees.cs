7f980f405eedb8301246e93f9a1bced4d9af48c8
STU6266 <stevenkemendics@gmail.com>
2026-06-11 20:28:50 +0200

    modified:   week06/code/Trees.cs

--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 public static class Trees
 {
     /// <summary>
@@ -48,6 +50,16 @@ public static class Trees
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
-        // TODO Start Problem 5
+        if (first > last)
+        {
+            return;
+        }
+
+        var middle = (first + last) / 2;
+
+        bst.Insert(sortedNumbers[middle]);
+
+        InsertMiddle(sortedNumbers, first, middle - 1, bst);
+        InsertMiddle(sortedNumbers, middle + 1, last, bst);
     }
 }
